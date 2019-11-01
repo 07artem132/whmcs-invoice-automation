b0b516500b6b7d31dc49c87d5444920d08bd771b
Artem Ivanko <ivankoartem@gmail.com>
2019-11-01 14:13:03 +0200

    fix error invoice not found

--- hooks.php
+++ hooks.php
@@ -2,13 +2,26 @@
 /**
  *  Created by PhpStorm.
  *  User: Артём
+ *  Date time: 01.11.19 14:13
+ *
+ */
+
+/**
+ *  Created by PhpStorm.
+ *  User: Артём
  *  Date time: 15.08.19 22:50
  *
  */
+
 use WHMCS\Billing\Invoice\Item as InvoiceItem;
 
 add_hook('PreModuleTerminate', 99999999, function ($vars) {
     $InvoiceItem = InvoiceItem::where('relid', '=', $vars['params']['serviceid'])->orderBy('id', 'desc')->first();
+
+    if (empty($InvoiceItem)) {
+        return;
+    }
+
     $invoice = $InvoiceItem->invoice()->first();
 
     if ($invoice->items()->count() == 1) {
